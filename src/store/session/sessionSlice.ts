1966cf42fc18e836d6444049cf523b6ecff1d454
Yuta Hamaya <01yhamaya05@gmail.com>
2021-03-08 17:51:33 +0900

    Redux-persistの追加、sessionストアの設定

---
+++ src/store/session/sessionSlice.ts
@@ -0,0 +1,60 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+interface SessionState {
+  isLoggedIn: boolean;
+  user_id: string;
+  email: string;
+  password: string;
+  status: string;
+  sidebarOpen: boolean;
+  selectModalOpen: boolean;
+  // error: string;
+}
+
+const initialState = {
+  isLoggedIn: false,
+  user_id: "",
+  email: "",
+  password: "",
+  status: "",
+  sidebarOpen: true,
+  selectModalOpen: false
+};
+
+export const loginRequest = createAsyncThunk("session/LOGIN", async (args: { email: string; password: string }) => {
+  // const { email, password } = args;
+  // const response = await auth.signInWithEmailAndPassword(email, password)
+  const response = "login";
+  return response;
+});
+
+export const slice = createSlice({
+  name: "session",
+  initialState,
+  reducers: {
+    //サイドバー開閉管理
+    toggleSidebar: (state, action: { payload: boolean }) => {
+      console.log("action", action);
+      state.sidebarOpen = action.payload;
+    }
+  },
+  extraReducers: {
+    "session/login/pending": (state: SessionState, action) => {
+      state.status = "loading";
+      console.log("state.status:", state.status);
+    },
+    "session/login/fulfilled": (state: SessionState, action) => {
+      state.status = "succeeded";
+      console.log("state.status:", state.status);
+      state.isLoggedIn = true;
+    },
+    "session/login/rejected": (state: SessionState, action) => {
+      state.status = "failed";
+      console.log('action.error', action.error);
+    }
+  }
+});
+
+export const { toggleSidebar } = slice.actions;
+
+export default slice.reducer;
---
+++ src/store/store.ts
@@ -0,0 +1,40 @@
+import { configureStore, getDefaultMiddleware, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './reducers';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
+})
+
+export let persistor = persistStore(store)
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
