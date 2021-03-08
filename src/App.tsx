5ace6a930e09eee36b2a21afd9da0832831d15ef
Yuta Hamaya <01yhamaya05@gmail.com>
2021-03-08 19:18:12 +0900

    Cypressを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from './components/Pages/Home';
+import SignIn from './components/Pages/SignIn';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const isLoggedIn = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {isLoggedIn ? (
+          <>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+            </header>
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </>
+        ) : (
+          <>
+            <Switch>
+              <Route component={SignIn} />
+            </Switch>
+          </>
+        )}
+      </div>
+    </Router>
   );
 }
 
