2fc768937696c4e817e261faf904cbf808792478
Shun Eto <prog.shun0830@gmail.com>
2021-03-10 09:42:18 +0900

    prettierファイルの更新とメモファイル

---
+++ .prettierrc.memo.js
@@ -0,0 +1,12 @@
+const prettier = {
+  //  アロー関数のシングルparamsに対する括弧の付与
+  arrowParens: "always",
+  //  オブジェクトの末尾カンマの付与
+  trailingComma: true,
+  //  改行コードの扱いをどうするか auto : 既存の行末を維持
+  endOfLine: "auto",
+  //  single quote の使用
+  singleQuote: false,
+  //  セミコロンの強制
+  semi: true
+};
